ef52333a03ec41f6ec9f29ddafd3f2bdc0bdd2b5
GabryelKadmo <gabryelkadmo@gmail.com>
2025-04-27 23:26:15 -0300

    feat: adicionar metadados Open Graph para SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,20 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "KDM Tecnologia",
   description: "Melhor solução para o seu negócio",
+  openGraph: {
+    title: "KDM Tecnologia",
+    description: "Melhor solução para o seu negócio",
+    images: [
+      {
+        url: "https://media-for2-2.cdn.whatsapp.net/v/t61.24694-24/464576668_573410681952369_2131898454168276175_n.jpg?ccb=11-4&oh=01_Q5Aa1QHDTtD6WJktgKhW4qapSaFgpFf6INv1iTp7ruHvth-SAQ&oe=681B49DE&_nc_sid=5e03e0&_nc_cat=104", // URL da imagem que aparecerá
+        width: 1200,
+        height: 630,
+        alt: "Imagem representativa do seu site",
+      },
+    ],
+    url: "https://www.kadmo.tech",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
