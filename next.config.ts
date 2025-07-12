91f9c8804698aa00b33bef56b07e4a3c2ac5c8e0
GabryelKadmo <gabryelkadmo@gmail.com>
2025-07-12 15:33:41 -0300

    feat: adiciona redirecionamento para subdomínio form.kadmo.tech

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/form',
+        destination: 'https://forms.gle/VeVfXbfQAEU5iCcv5',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
